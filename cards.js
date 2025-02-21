cd551617c7a35fbd482502175e4e51dc009da6cd
Hruthvik Thota <sunnynetha23@gmail.com>
2025-02-21 20:56:26 +0530

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,14 @@
+let button=document.getElementById("btn")
+let c1=document.getElementById("c1")
+let c3=document.getElementById("c3")
+
+function clicked(){
+    c1.classList.toggle("c11")
+    c3.classList.toggle("c33")
+}
+
+// let container=document.getElementById("contianer")
+
+// function clicked(){
+//     container.classList.toggle("pos")
+// }
